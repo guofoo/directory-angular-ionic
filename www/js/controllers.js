ac7e0ae53d7907529d8414e3937d507459ea1f80
meticulous-dft <meticulous.dft@gmail.com>
2015-01-28 08:51:04 +0000

    add grunt server

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,34 +1,34 @@
 angular.module('directory.controllers', [])
 
-  .controller('EmployeeIndexCtrl', function ($scope, EmployeeService) {
+.controller('EmployeeIndexCtrl', function ($scope, EmployeeService) {
 
+  $scope.searchKey = "";
+
+  $scope.clearSearch = function () {
     $scope.searchKey = "";
-
-    $scope.clearSearch = function () {
-      $scope.searchKey = "";
-      findAllEmployees();
-    };
-
-    $scope.search = function () {
-      EmployeeService.findByName($scope.searchKey).then(function (employees) {
-        $scope.employees = employees;
-      });
-    };
-
-    var findAllEmployees = function() {
-      EmployeeService.findAll().then(function (employees) {
-        $scope.employees = employees;
-      });
-    };
-
     findAllEmployees();
+  };
 
-  })
-    
-  .controller('EmployeeDetailCtrl', function($scope, $stateParams, EmployeeService) {
-    EmployeeService.findById($stateParams.employeeId).then(function (employee) {
-      $scope.employee = employee;  
+  $scope.search = function () {
+    EmployeeService.findByName($scope.searchKey).then(function (employees) {
+      $scope.employees = employees;
     });
-  })    
-    
+  };
+
+  var findAllEmployees = function() {
+    EmployeeService.findAll().then(function (employees) {
+      $scope.employees = employees;
+    });
+  };
+
+  findAllEmployees();
+
+})
+  
+.controller('EmployeeDetailCtrl', function($scope, $stateParams, EmployeeService) {
+  EmployeeService.findById($stateParams.employeeId).then(function (employee) {
+    $scope.employee = employee;  
+  });
+})    
+  
 ;
