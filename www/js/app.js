5854c1cf80781db5efe68403f35584c3c00af178
meticulous-dft <meticulous.dft@gmail.com>
2015-01-28 09:17:20 +0000

    add grunt server

--- www/js/app.js
+++ www/js/app.js
@@ -5,59 +5,59 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('directory', ['ionic','directory.services','directory.controllers'])
 
- .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
-    $stateProvider
-    
-    .state('employees', {
-      url: '/employees',
-      templateUrl: 'templates/employee-index.html',
-      controller: 'EmployeeIndexCtrl'
-    })
-    
-    .state('employee', {
-      url: '/employees/:employeeId',
-      templateUrl: 'templates/employee-detail.html',
-      controller: 'EmployeeDetailCtrl',
-      resolve: {
-        employee: ['EmployeeService', '$stateParams', function(EmployeeService, $stateParams){
-          return EmployeeService.findById($stateParams.employeeId);
-        }]
-      }
-    })  
-
-    .state('reports', {
-      url: '/employees/:employeeId/reports',
-      templateUrl: 'templates/employee-reports.html',
-      controller: 'EmployeeReportsCtrl',
-      resolve: {
-        employee: ['EmployeeService', '$stateParams', function(EmployeeService, $stateParams){
-          return EmployeeService.findById($stateParams.employeeId);
-        }],
-        reports: ['EmployeeService', '$stateParams', function(EmployeeService, $stateParams){
-          return EmployeeService.findByManager($stateParams.employeeId);
-        }]
-      }
-    })
-    
-    ;
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+  $stateProvider
   
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/employees');   
- }])
+  .state('employees', {
+    url: '/employees',
+    templateUrl: 'templates/employee-index.html',
+    controller: 'EmployeeIndexCtrl'
+  })
+  
+  .state('employee', {
+    url: '/employees/:employeeId',
+    templateUrl: 'templates/employee-detail.html',
+    controller: 'EmployeeDetailCtrl',
+    resolve: {
+      employee: ['EmployeeService', '$stateParams', function(EmployeeService, $stateParams){
+        return EmployeeService.findById($stateParams.employeeId);
+      }]
+    }
+  })  
+
+  .state('reports', {
+    url: '/employees/:employeeId/reports',
+    templateUrl: 'templates/employee-reports.html',
+    controller: 'EmployeeReportsCtrl',
+    resolve: {
+      employee: ['EmployeeService', '$stateParams', function(EmployeeService, $stateParams){
+        return EmployeeService.findById($stateParams.employeeId);
+      }],
+      reports: ['EmployeeService', '$stateParams', function(EmployeeService, $stateParams){
+        return EmployeeService.findByManager($stateParams.employeeId);
+      }]
+    }
+  })
+  
+  ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/employees');   
+}])
 
 .run(function($ionicPlatform) {
-    $ionicPlatform.ready(function() {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      if(window.cordova && window.cordova.plugins.Keyboard) {
-        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      }
-      
-      if(window.StatusBar) {
-        StatusBar.styleDefault();
-      }
-    });
-  
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+
 })
 
 ;
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,38 +1,38 @@
 angular.module('directory.controllers', [])
 
-  .controller('EmployeeIndexCtrl', function ($scope, EmployeeService) {
+.controller('EmployeeIndexCtrl', function ($scope, EmployeeService) {
 
+  $scope.searchKey = "";
+
+  $scope.clearSearch = function () {
     $scope.searchKey = "";
-
-    $scope.clearSearch = function () {
-      $scope.searchKey = "";
-      findAllEmployees();
-    };
-
-    $scope.search = function () {
-      EmployeeService.findByName($scope.searchKey).then(function (employees) {
-        $scope.employees = employees;
-      });
-    };
-
-    var findAllEmployees = function() {
-      EmployeeService.findAll().then(function (employees) {
-        $scope.employees = employees;
-      });
-    };
-
     findAllEmployees();
+  };
 
-  })
-    
-  .controller('EmployeeDetailCtrl', function($scope, employee) {
-    $scope.employee = employee;  
-  })    
-   
-  .controller('EmployeeReportsCtrl', function ($scope, employee, reports) {
-    $scope.employee = employee;
-    $scope.reports = reports;
-  })
+  $scope.search = function () {
+    EmployeeService.findByName($scope.searchKey).then(function (employees) {
+      $scope.employees = employees;
+    });
+  };
+
+  var findAllEmployees = function() {
+    EmployeeService.findAll().then(function (employees) {
+      $scope.employees = employees;
+    });
+  };
+
+  findAllEmployees();
+
+})
+  
+.controller('EmployeeDetailCtrl', function($scope, employee) {
+  $scope.employee = employee;  
+})    
+ 
+.controller('EmployeeReportsCtrl', function ($scope, employee, reports) {
+  $scope.employee = employee;
+  $scope.reports = reports;
+})
    
     
 ;
